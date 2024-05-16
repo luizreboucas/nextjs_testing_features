2708e6ce3abb6fc8ac0ba4711e1b37cae1a23d72
luiz.reboucas <luiz.reboucas@hostweb.com.br>
2024-05-16 13:38:51 -0300

    first commit

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -3,16 +3,25 @@ import getPosts from '@/lib/getUsersPost'
 import React from 'react'
 import { Suspense } from 'react'
 import UserPosts from './components/UserPosts'
+import { Metadata } from 'next'
 type Params = {
     params: {
        userId: string
     }
 }
+export async function generateMetadata({params: {userId}}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId);
+  const user: User = await userData;
+  return {
+    title: user.name,
+    description: `this is the page of the user ${user.id}`
+  }
+}
 
 export default async function UserPage({params: {userId}}: Params) {
-    const userData: Promise<User> = getUser(userId);
+   
     const postData: Promise<any[]> = getPosts(userId);
-    const user = await userData
+    const user = await getUser(userId);
   return (
     <>
     
